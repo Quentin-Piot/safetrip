759cca253b09599911fb16072ea2f6587db0de91
Quentin Piot <qkpiot@gmail.com>
2019-12-08 16:10:03 +0900

    SafeTrip is now impossible to close

---
+++ app/src/main/java/com/team8/safetrip/BackgroundService.kt
@@ -0,0 +1,12 @@
+package com.team8.safetrip
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class BackgroundService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
